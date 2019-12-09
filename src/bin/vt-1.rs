3aa2f1619177e9c7e1127fcc733b04622236d063
cynic64 <f3bl@gmx.com>
2019-12-09 17:56:48 +0100

    vt-1 includes a proper validation message callback

--- src/bin/vt-1.rs
+++ src/bin/vt-1.rs
@@ -2,7 +2,10 @@ use ash::extensions::khr::XlibSurface;
 use ash::extensions::{ext::DebugReport, khr::Surface};
 use ash::version::{EntryV1_0, InstanceV1_0};
 use ash::{vk, vk_make_version, Entry};
-use std::ffi::CString;
+
+use std::ffi::{CStr, CString};
+use std::os::raw::c_void;
+use std::ptr;
 
 use winit::{Event, WindowEvent};
 
@@ -47,10 +50,26 @@ pub fn main() {
 
     let extension_names_raw = extension_names();
 
+    let mut debug_utils_create_info = vk::DebugUtilsMessengerCreateInfoEXT {
+        s_type: vk::StructureType::DEBUG_UTILS_MESSENGER_CREATE_INFO_EXT,
+        p_next: ptr::null(),
+        flags: vk::DebugUtilsMessengerCreateFlagsEXT::empty(),
+        message_severity: vk::DebugUtilsMessageSeverityFlagsEXT::WARNING
+            | vk::DebugUtilsMessageSeverityFlagsEXT::VERBOSE
+            | vk::DebugUtilsMessageSeverityFlagsEXT::INFO
+            | vk::DebugUtilsMessageSeverityFlagsEXT::ERROR,
+        message_type: vk::DebugUtilsMessageTypeFlagsEXT::GENERAL
+            | vk::DebugUtilsMessageTypeFlagsEXT::PERFORMANCE
+            | vk::DebugUtilsMessageTypeFlagsEXT::VALIDATION,
+        pfn_user_callback: Some(vulkan_debug_utils_callback),
+        p_user_data: ptr::null_mut(),
+    };
+
     let create_info = vk::InstanceCreateInfo::builder()
         .application_info(&app_info)
         .enabled_layer_names(&layers_names_raw)
-        .enabled_extension_names(&extension_names_raw);
+        .enabled_extension_names(&extension_names_raw)
+        .push_next(&mut debug_utils_create_info);
 
     let entry = Entry::new().unwrap();
 
@@ -60,6 +79,7 @@ pub fn main() {
             .expect("Couldn't create instance")
     };
 
+    // print info about instance
     unsafe {
         instance
             .enumerate_physical_devices()
@@ -81,4 +101,29 @@ fn extension_names() -> Vec<*const i8> {
         XlibSurface::name().as_ptr(),
         DebugReport::name().as_ptr(),
     ]
+}
+
+unsafe extern "system" fn vulkan_debug_utils_callback(
+    message_severity: vk::DebugUtilsMessageSeverityFlagsEXT,
+    message_type: vk::DebugUtilsMessageTypeFlagsEXT,
+    p_callback_data: *const vk::DebugUtilsMessengerCallbackDataEXT,
+    _p_user_data: *mut c_void,
+) -> vk::Bool32 {
+    let severity = match message_severity {
+        vk::DebugUtilsMessageSeverityFlagsEXT::VERBOSE => "[Verbose]",
+        vk::DebugUtilsMessageSeverityFlagsEXT::WARNING => "[Warning]",
+        vk::DebugUtilsMessageSeverityFlagsEXT::ERROR => "[Error]",
+        vk::DebugUtilsMessageSeverityFlagsEXT::INFO => "[Info]",
+        _ => "[Unknown]",
+    };
+    let types = match message_type {
+        vk::DebugUtilsMessageTypeFlagsEXT::GENERAL => "[General]",
+        vk::DebugUtilsMessageTypeFlagsEXT::PERFORMANCE => "[Performance]",
+        vk::DebugUtilsMessageTypeFlagsEXT::VALIDATION => "[Validation]",
+        _ => "[Unknown]",
+    };
+    let message = CStr::from_ptr((*p_callback_data).p_message);
+    println!("[Debug]{}{}{:?}", severity, types, message);
+
+    vk::FALSE
 }
